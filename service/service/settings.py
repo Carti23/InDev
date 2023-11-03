1366baeef35f956629c65d1194e70664b1c5a2b5
Roman <roman@Romans-MacBook-Air-2.local>
2023-11-03 16:33:42 +0200

    add swagger

--- service/service/settings.py
+++ service/service/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     # Third party apps
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'rest_framework_swagger',
+    'drf_yasg',
 
     # My apps
     'authentication',
--- service/service/urls.py
+++ service/service/urls.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 
-schema_view = get_swagger_view(title='Staking API')
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API Title",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 api_urlpatterns = [
@@ -15,6 +28,9 @@ api_urlpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
-    path('swagger/', schema_view),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('api/base-auth/', include('rest_framework.urls')),
 ]
